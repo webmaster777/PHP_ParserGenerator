831ceb8978d502b75c3d9ec23f159bf1d968e36c
François Poirotte <clicky@erebot.net>
2018-05-10 16:50:06 +0200

    Big cleanup
    Remove unneeded files & add export exceptions to .gitattributes

--- examples/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'PHP/ParserGenerator.php';
-$a = new PHP_ParserGenerator;
-$_SERVER['argv'] = array('lemon', '-s', '/development/lemon/PHP_Parser.y');
-//$_SERVER['argv'] = array('lemon', '-s', '/development/File_ChessPGN/ChessPGN/Parser.y');
-$a->main();
--- oops.y
+++
@@ -1,9 +0,0 @@
-common_scalar(A) ::=
-		T_LNUMBER
-	   |T_DNUMBER
-	   |T_CONSTANT_ENCAPSED_STRING
-	   |T_LINE
-	   |T_FILE
-	   |T_CLASS_C
-	   |T_METHOD_C
-	   |T_FUNC_C(B). {A = B;}
